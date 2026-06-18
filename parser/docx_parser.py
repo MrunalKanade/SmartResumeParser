633f9b50f628a49041fdd9047ef0ea8e037f9910
MrunalKanade <mrunalk2504@gmail.com>
2026-06-18 17:02:27 +0530

    Added DOCX resume support

---
+++ parser/docx_parser.py
@@ -0,0 +1,13 @@
+from docx import Document
+
+
+def extract_text_from_docx(docx_file):
+
+    doc = Document(docx_file)
+
+    text = ""
+
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+
+    return text
