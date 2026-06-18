0fae2d7b30baad8cc050328030d824972e9f1fc9
MrunalKanade <mrunalk2504@gmail.com>
2026-06-18 16:19:37 +0530

    Added PDF text extraction module

---
+++ parser/pdf_parser.py
@@ -0,0 +1,19 @@
+import fitz
+
+
+def extract_text_from_pdf(pdf_file):
+    """
+    Extract text from uploaded PDF.
+    """
+
+    text = ""
+
+    pdf = fitz.open(
+        stream=pdf_file.read(),
+        filetype="pdf"
+    )
+
+    for page in pdf:
+        text += page.get_text()
+
+    return text
